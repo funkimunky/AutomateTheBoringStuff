e2dd8f467ce45fe221ca6f955bb2a6dae9bd76f2
funkimunky <davidthomson123@hotmail.com>
2024-02-09 13:15:46 +0000

    Add mapit script

---
+++ mapit.bat
@@ -0,0 +1,2 @@
+@C:\programming\python\AutomateTheBoringStuff\.venv\Scripts\python.exe C:\programming\python\AutomateTheBoringStuff\mapit.py %*
+@pause
---
+++ mapit.py
@@ -0,0 +1,13 @@
+import webbrowser
+import sys
+import pyperclip
+
+parameters = sys.argv
+
+if len(parameters) > 1:
+    address = ' '.join(parameters[1:])
+else:
+    address = pyperclip.paste()
+
+# https://www.google.com/maps/place/26+Ash+Grove,+Perth+PH1+1DR/@56.3940755,-3.4833203,17z/data=!3m1!4b1!4m6!3m5!1s0x488624cf2333a217:0x6031fbe57879a4c!8m2!3d56.3940726!4d-3.4807454!16s%2Fg%2F11c259jnl0?entry=ttu
+webbrowser.open('https://www.google.com/maps/place/' + address)
