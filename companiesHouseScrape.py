3d89a0aaf44c84c3aaff859b0fb1d0f5014c1953
David Thomson <davidthomson123@hotmail.com>
2024-02-16 08:55:20 +0000

    added companies house url

--- companiesHouseScrape.py
+++ companiesHouseScrape.py
@@ -22,16 +22,15 @@ import requests
 # list people in company
 # https://find-and-update.company-information.service.gov.uk/company/SC190800/officers
 
-url = 'https://www.ebay.co.uk'
-search_item = 'planner+thicknesser'
-search_url = '/sch/i.html?_nkw='+search_item+'&_sacat=0&_blrs=spell_auto_correct'
+companyNumber = 'SC190800'
+search_url = 'https://find-and-update.company-information.service.gov.uk/company/'
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
 
-res = requests.get(url+search_url, headers=headers)
+res = requests.get(search_url, headers=headers)
 res.raise_for_status()
 soup = BeautifulSoup(res.text, 'html.parser')
 
 elems = soup.select('ul.srp-results li')
-print(elems[0].text)
+
 
 
